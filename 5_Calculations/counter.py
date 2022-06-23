5ed01bb226272c0ec10a49a3b0748bf416e6f154
[Katerina Sandoval] <105500680+Kfsand@users.noreply.github.com>
2022-06-23 12:32:32 +0100

    functional counter except last iteration on periodizer

--- 5_Calculations/counter.py
+++ 5_Calculations/counter.py
@@ -6,12 +6,28 @@ import numpy as np
 def var_counter(array3D,threshold=6,periodiser=20):
 
     exc_array=islarger(array3D,threshold)
-    counter=np.empty((20,22,13))
+    counter=np.empty((12*periodiser,np.size(array3D,1),np.size(array3D,2)))
+    print(counter.shape)
 
-    for i in range(3):
-        idx_1 = periodiser*i
-        idx_2 = periodiser*(i+1)
-        np.add(counter,exc_array[idx_1:idx_2,:,:].astype(int))
+    for i in range(5):
+        #1199//(periodiser*12)
+        print(i)
+        zeros=np.zeros(counter.shape)
+        idx_1 = periodiser*12*i
+        idx_2 = periodiser*12*(i+1)
+        slice=exc_array[idx_1:idx_2,:,:]
+
+        if counter.shape== slice.shape:
+            print(slice.shape)
+            np.add(counter,slice.astype(int))
+            
+        else:
+            'TODO: fix last iteration'
+            continue
+            resized=exc_array[idx_1:idx_2,:,:].resize(counter.shape)
+            print(resized.shape)
+            np.add(counter,resized.astype(int))
+        
 
     return counter
 
