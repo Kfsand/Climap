97239817dccf67f3233a8791be06ea50478c7b9a
[Katerina Sandoval] <105500680+Kfsand@users.noreply.github.com>
2022-07-08 10:47:59 +0200

    corod writing into geojson made function

--- 5_Calculations/mapping.py
+++ 5_Calculations/mapping.py
@@ -1,6 +1,8 @@
 #Functions useful for coordinate conversion and geojson structuring
 from bng_latlon import OSGB36toWGS84
 import numpy as np
+import pandas as pd
+import json
 
 
 def ReuCoord (xcoord,ycoord):
@@ -16,7 +18,38 @@ def bulkOSGB36toWGS84(xBNG, yBNG):
 
     for i in xBNG:
         for j in yBNG:
-            #print(OSGB36toWGS84(i, j))
             latlonarray.append(OSGB36toWGS84(i, j))
 
     return latlonarray
+
+
+def CoordtoGeojson(latlonarray,fname='dataset.json'):
+    'Note: convert data to geojson and write in file'
+
+    df = pd.DataFrame(latlonarray, columns=['latitude','longitude'])
+
+
+    def df_to_geojson(df, properties, lat='latitude', lon='longitude'):
+        geojson = {'type':'FeatureCollection', 'features':[]}
+        for _, row in df.iterrows():
+            feature = {'type':'Feature',
+                    'properties':{},
+                    'geometry':{'type':'Point',
+                                'coordinates':[]}}
+            feature['geometry']['coordinates'] = [row[lon],row[lat]]
+            for prop in properties:
+                feature['properties'][prop] = row[prop]
+            geojson['features'].append(feature)
+        return geojson
+
+
+    cols = ['latitude','longitude']
+    geojson = df_to_geojson(df, cols)
+
+
+    output_filename = '../6_Results/data/' +fname
+    with open(output_filename, 'w') as output_file:
+        json.dump(geojson, output_file, indent=2)
+    
+    pass
+
