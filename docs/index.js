1cd3d7f1957d0755856c459f0b94fce10afc4adb
[Katerina Sandoval] <105500680+Kfsand@users.noreply.github.com>
2022-09-13 22:52:07 +0100

    page deployement

---
+++ docs/index.js
@@ -0,0 +1,25 @@
+function hide (){
+
+    //divs to hide
+    $("#menu").hide()
+
+    //button
+    $("#showUI").show()
+    $("#hideUI").hide()
+}
+
+function show (){
+
+    $("#menu").show()
+
+    //button
+    $("#showUI").hide()
+    $("#hideUI").show()
+
+}
+
+function changeMap(){
+
+    alert(" in change map")
+
+}
